6e75fbd18bb914b999d8d877c98194002726d8ce
Mr.L@mbert_13 <lam_13@mail.ru>
2019-07-31 00:34:32 +0300

    add comment

--- amocrm/settings.php
+++ amocrm/settings.php
@@ -1,5 +1,9 @@
 <?php
-// POST parameters
+/*
+* Parameters for apps. Change 'USER_LOGIN', 'USER_HASH', and 'subdomain' for using.
+*
+* Created by Mr.L@mbert_13
+*/
 return [
   'user' => [
     'USER_LOGIN' => 'lam_13@mail.ru',
@@ -13,25 +17,3 @@ return [
     'tasks' => '/api/v2/tasks'
   ],
 ];
-// $user = array(
-//   'USER_LOGIN' => 'lam_13@mail.ru',
-//   'USER_HASH' => '1e5f912661b6949e0fa66fb3c3d30550db62d7a2',
-// );
-// $subdomain = 'lam13';
-// $domain = '.amocrm.ru';
-// $API = [
-//   'auth' => "{$domain}/private/api/auth.php?type=json",
-//   'leads' => "{$domain}/api/v2/leads",
-//   'tasks' => "{$domain}/api/v2/tasks"
-// ];
-
-// $errors = array(
-//   301 => 'Moved permanently',
-//   400 => 'Bad request',
-//   401 => 'Unauthorized',
-//   403 => 'Forbidden',
-//   404 => 'Not found',
-//   500 => 'Internal server error',
-//   502 => 'Bad gateway',
-//   503 => 'Service unavailable',
-// );
